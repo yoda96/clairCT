b1899bd85c27537ac61c9d6dc1b498236eb4d7f5
yoda96 <dnyaneshwarmundhe15@gmail.com>
2019-01-24 17:49:23 +0530

    Update slack.sh

--- slack.sh
+++ slack.sh
@@ -1,1 +1,1 @@
-curl -X POST -d 'payload={"text":"Message to be displayed"}' https://hooks.slack.com/services/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
+curl -X POST -d 'payload={"text":"Message to be displayed:Execute order 66"}' https://hooks.slack.com/services/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
